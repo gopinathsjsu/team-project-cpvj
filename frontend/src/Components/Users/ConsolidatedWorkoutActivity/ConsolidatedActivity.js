99c620454b2974e2911c4f96897d9e27e4c6b1a9
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-04-16 00:43:03 -0700

    consolidated activity setup

---
+++ frontend/src/Components/Users/ConsolidatedWorkoutActivity/ConsolidatedActivity.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+import {Card, Grid, Spacer, Tooltip} from "@nextui-org/react";
+
+const ConsolidatedActivity = ()=>{
+    // console.log(props.data);
+    const [data, setData] = useState([]);
+
+    // console.log(data);
+
+
+    // console.log(len);
+
+
+    /**
+     *
+     * lets have 3 intensities of green color
+     * #AFE1AF => for 15 to 30 mins
+     * #50C878 => for 30 to 60 mins
+     * #4F7942 => for 60 mins+
+     *
+     *
+     *
+     */
+
+}
+
+
+export default ConsolidatedActivity
