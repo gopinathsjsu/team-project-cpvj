77641d4af9302cce24dfce5935159fc6c00ae6da
priyanka <priyanka1012madas@gmail.com>
2023-04-16 23:43:45 -0700

    setup state

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -5,6 +5,27 @@ import {useNavigate} from "react-router-dom";
 
 const Login = ()=>{
 
+    const navigate = useNavigate();
+    let mes = false;
+    let errorMessage = "";
+    const [error, setError] = useState(mes);
+
+    const [message, setMessage] = useState(errorMessage);
+
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
+
+    // const el = {
+    //     id: 1,
+    //     locationid:0,
+    //     name: "Chiruhas",
+    //     email:"chiruhas@gmail.com"
+    // }
+    // sessionStorage.setItem('employee', JSON.stringify(el));
 
 
 }
