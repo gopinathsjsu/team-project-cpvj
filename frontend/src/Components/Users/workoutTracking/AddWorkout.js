3b70c36c38cf7f02b6781645f51d113ac8af94e2
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-04-21 00:49:37 -0700

    data submit handler completion in add workout

--- frontend/src/Components/Users/workoutTracking/AddWorkout.js
+++ frontend/src/Components/Users/workoutTracking/AddWorkout.js
@@ -13,6 +13,41 @@ const AddWorkout = ()=> {
         [workout]
     );
 
+    const [loading, setLoading] = useState(false);
+
+    const el = loading ? <Button type="submit"><Loading color='success'/></Button> :
+        <Button type="submit">Submit</Button>
+
+    const user = JSON.parse(sessionStorage.user)
+
+
+    const formSubmitHandler = (event)=>{
+        event.preventDefault()
+
+
+        const type = event.target.workoutType.children[1].innerHTML
+
+        console.log(type);
+
+        if(type==="Choose a Workout")
+            alert("Choose a workout type");
+        else{
+
+
+            let dp = workouts.filter((el) => {
+                return el.key === type
+            })
+
+            const data = {
+                userid:user.id,
+                deviceid:dp[0].value,
+                start:event.target.startTime.value,
+                end:event.target.endTime.value
+            }
+            console.log(data);
+            apiCall(data)
+        }
+    }
 
 
 }
