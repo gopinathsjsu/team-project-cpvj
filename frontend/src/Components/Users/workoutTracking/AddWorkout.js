2d0f37ab0c82f332ed0eabf2c09a242ba6997536
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-04-22 00:51:39 -0700

    add workout activity completed

--- frontend/src/Components/Users/workoutTracking/AddWorkout.js
+++ frontend/src/Components/Users/workoutTracking/AddWorkout.js
@@ -48,6 +48,88 @@ const AddWorkout = ()=> {
             apiCall(data)
         }
     }
+    const apiCall = async (data)=>{
+
+
+        setLoading(true);
+
+        try{
+            console.log(JSON.stringify(data));
+            await axios.post("http://100.26.42.194:8080/userActivity", JSON.stringify(data) )
+            alert("Data Submitted sucessfully")
+
+
+
+        }catch (e){
+            console.log(e);
+            alert("OOps something happened")
+        }
+
+        setLoading(false);
+
+    }
+
+    return  <Card isHoverable variant="bordered" >
+        <Card.Body>
+            <form onSubmit={formSubmitHandler}>
+                <Grid.Container gap={2} >
+
+                    <Grid xs={12} justify="center">
+                        <Text h3>Track your workout.</Text>
+                    </Grid>
+
+                    <Grid xs={12} justify="center">
+                        <Dropdown name="location">
+                            <Dropdown.Button flat color="secondary" css={{tt: "capitalize"}} name='workoutType' required>
+                                {workoutValue}
+                            </Dropdown.Button>
+                            <Dropdown.Menu
+                                aria-label="Single selection actions"
+                                color="secondary"
+                                disallowEmptySelection
+                                selectionMode="single"
+                                selectedKeys={workouts}
+                                onSelectionChange={setWorkout}
+                                items={workouts}
+
+                            >
+
+                                {(item) => (
+                                    <Dropdown.Item>
+                                        {item.key}
+                                    </Dropdown.Item>
+                                )}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Grid>
+                    <Grid xs={6} justify="center">
+                        <Input
+                            clearable bordered
+                            label="Start Time"
+                            type="time"
+                            name="startTime"
+                            required
+                        />
+                    </Grid>
+                    <Grid xs={6} justify="center">
+                        <Input
+                            clearable bordered
+                            label="End Time"
+                            type="time"
+                            name="endTime"
+                            required
+                        />
+                    </Grid>
+                    <Grid xs={12} justify="center">
+                        {el}
+                    </Grid>
+
+
+                </Grid.Container>
+            </form>
+
+        </Card.Body>
+    </Card>
 
 
 }
