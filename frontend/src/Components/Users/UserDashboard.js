a720313520e19955939b2758025fee30f313bbc8
priyanka <priyanka1012madas@gmail.com>
2023-04-24 14:38:33 -0700

    Added UserDashboard

---
+++ frontend/src/Components/Users/UserDashboard.js
@@ -0,0 +1,15 @@
+import {Card, Grid, Spacer, Text} from "@nextui-org/react";
+import CustomerNavbar from "./Navbar/CustomerNavbar";
+import AddWorkout from "./workoutTracking/AddWorkout";
+import React, {useEffect, useState} from "react";
+import WorkoutDashboard from "./workoutTracking/WorkoutDashboard";
+import UpcomingClassCard from "./Classes/UpcomingClassCard";
+import ConsolidatedActivity from "./ConsolidatedWorkoutActivity/ConsolidatedActivity";
+import EnrollClass from "./Classes/EnrollClass/EnrollClass";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+const UserDashboard = () =>{
+
+}
+export default UserDashboard;
