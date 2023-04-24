2167ec43800123fd7efbdd8d315e50eb835f8cd9
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-04-24 11:56:42 -0700

    setup workoutDashboard

---
+++ frontend/src/Components/Users/workoutTracking/WorkoutDashboard.js
@@ -0,0 +1,17 @@
+import {Container, Dropdown, Grid, Text} from "@nextui-org/react";
+import React, {useEffect} from "react";
+import axios from "axios";
+
+const WorkoutDashboard = (props) => {
+
+    const days = [{key:"1 day"},{key:"7 days"},{key:"30 days"},{key:"60 days"},{key : "90 days"}];
+    const [day, setDay] = React.useState(new Set(["1 day"]));
+    const [selectedDay, setSelectedDay] = React.useState("1 day");
+
+
+
+
+}
+
+
+export default WorkoutDashboard
