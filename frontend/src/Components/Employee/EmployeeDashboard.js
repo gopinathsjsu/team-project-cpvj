6f2131f8c33f4d6ccd7220cdae4d766032ab70e2
Jaswanth <jaswanthkarangula15@gmail.com>
2023-04-27 17:48:14 -0700

    Employee Dashboard

---
+++ frontend/src/Components/Employee/EmployeeDashboard.js
@@ -0,0 +1,60 @@
+import {Card, Grid, Spacer, Text} from "@nextui-org/react";
+import EmployeeNavbar from "./Navbar/EmployeeNavbar";
+import Analytics from "./Sections/analytics/Analytics";
+import ClassManagement from "./Sections/ClassManagement";
+import UserManagement from "./Sections/UserManagement";
+import ClassInfo from "./viewClasses/ClassInfo";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+
+
+
+const EmployeeDashboard = () => {
+
+
+    const navigate = useNavigate();
+
+
+
+
+    useEffect(()=>{
+
+
+
+
+
+
+
+    },[])
+
+   let employee = e
+
+    return <>
+        {/*todo*/}
+        {/*Fetch user name and pass it down as prop.*/}
+        <EmployeeNavbar name={employee.name} isActive='analytics'/>
+
+
+        <Analytics/>
+
+
+        <Grid.Container gap={2}>
+            <Grid xs={3}></Grid>
+            <Grid xs={3}><ClassManagement/></Grid>
+            <Grid xs={3}><UserManagement/></Grid>
+            <Grid xs={3}></Grid>
+
+        </Grid.Container>
+
+        <ClassInfo/>
+
+
+
+    </>
+
+
+}
+
+
+export default EmployeeDashboard;
