82ca16ff61e4aa38b07a24cbc3d8bcad00eab0fe
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-04-29 15:17:16 -0700

    api call integration for analytics

--- frontend/src/Components/Employee/Sections/analytics/Graphs/Analytics.js
+++ frontend/src/Components/Employee/Sections/analytics/Graphs/Analytics.js
@@ -10,6 +10,48 @@ const Analytics = () => {
 
     const [selectedChoice, setSelectedChoice] = useState("Daily");
 
+    const getData = async () => {
+
+        let d=""
+        selected.forEach((value) =>{
+            d = value
+        });
+        let dp = choices.filter((el) => {
+            return el.key === d
+        })
+        dp = dp[0].value+1
+
+        let { data } = await axios.get(`http://100.26.42.194:8080/getOverallAnalytics?type=${dp}`);
+
+        data = JSON.parse(data)
+
+        setData((d)=>{
+
+            let t=[]
+
+
+
+
+            for (let i = 0; i < 3; i++) {
+
+                let p = Math.round(((data[i].current-data[i].previous)/data[i].current*100.0)||0)
+                let o={current : data[i].current, previous:data[i].previous, change :p}
+                t.push(o)
+            }
+
+            console.log(t);
+
+
+            return t;
+
+
+        });
+    };
+
+    useEffect(()=>{
+        getData()
+    },[])
+
 
 }
 
