cf5357082e73b24d716c220e99f55698959cafdb
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-04-30 16:21:36 -0700

    code reformat

--- frontend/src/Components/Employee/Sections/analytics/Graphs/Analytics.js
+++ frontend/src/Components/Employee/Sections/analytics/Graphs/Analytics.js
@@ -1,19 +1,21 @@
-
-import AnalyticsCard from "./AnalyticsCard";
-import {Dropdown, Grid, Text} from "@nextui-org/react";
+import AnalyticsCards from "./AnalyticsCard";
+import {Dropdown, Grid} from "@nextui-org/react";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import WeeklyDayVsClassesLine from "./Graphs/WeeklyDayVsClassesLine";
 import HoursSpentByWeekType from "./Graphs/HoursSpentByWeekType";
-import AnalyticsCards from "./AnalyticsCard";
 import ClassesVsUsers from "./Graphs/ClassesVsUsers";
 import MostVisitedDayHeatMap from "./Graphs/HeatMap/MostVisitedDayHeatMap";
 
 const Analytics = () => {
 
-    const [data, setData] = useState([{previous:0,current:0,change:0},{previous:0,current:0,change:0},{previous:0,current:0,change:0}]);
+    const [data, setData] = useState([{previous: 0, current: 0, change: 0}, {
+        previous: 0,
+        current: 0,
+        change: 0
+    }, {previous: 0, current: 0, change: 0}]);
 
-    const choices=[{key:"Daily",value:0},{key:"Weekly",value:1},{key:"Monthly",value: 2}]
+    const choices = [{key: "Daily", value: 0}, {key: "Weekly", value: 1}, {key: "Monthly", value: 2}]
 
     const [selected, setSelected] = React.useState(new Set(["Daily"]));
 
@@ -21,30 +23,28 @@ const Analytics = () => {
 
     const getData = async () => {
 
-        let d=""
-        selected.forEach((value) =>{
+        let d = ""
+        selected.forEach((value) => {
             d = value
         });
         let dp = choices.filter((el) => {
             return el.key === d
         })
-        dp = dp[0].value+1
+        dp = dp[0].value + 1
 
-        let { data } = await axios.get(`http://100.26.42.194:8080/getOverallAnalytics?type=${dp}`);
+        let {data} = await axios.get(`http://100.26.42.194:8080/getOverallAnalytics?type=${dp}`);
 
         data = JSON.parse(data)
 
-        setData((d)=>{
+        setData((d) => {
 
-            let t=[]
-
-
+            let t = []
 
 
             for (let i = 0; i < 3; i++) {
 
-                let p = Math.round(((data[i].current-data[i].previous)/data[i].current*100.0)||0)
-                let o={current : data[i].current, previous:data[i].previous, change :p}
+                let p = Math.round(((data[i].current - data[i].previous) / data[i].current * 100.0) || 0)
+                let o = {current: data[i].current, previous: data[i].previous, change: p}
                 t.push(o)
             }
 
@@ -57,9 +57,9 @@ const Analytics = () => {
         });
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getData()
-    },[])
+    }, [])
 
 
     return <Grid.Container gap={2}>
