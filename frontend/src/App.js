91ce64308bf369536dff74c0f48aa5e2ee0e80d8
Venkata Subramanya Sri Raviraj Indraganti <111710961+ivssraviraj@users.noreply.github.com>
2023-04-30 23:08:09 -0700

    Added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {createTheme, NextUIProvider} from '@nextui-org/react';
+import EmployeeDashboard from "./Components/Employee/EmployeeDashboard";
+import CustomerDashboard from "./Components/Users/UserDashboard";
+import Login from "./Components/Login/Login";
+
+
+const theme = createTheme({
+    type: "dark", // it could be "light" or "dark"
+    theme: {
+        colors: {
+            // brand colors
+            primaryLight: '$green200',
+            primaryLightHover: '$green300',
+            primaryLightActive: '$green400',
+            primaryLightContrast: '$green600',
+            primary: '#0072F5',
+            primaryBorder: '$green500',
+            primaryBorderHover: '$green600',
+            primarySolidHover: '$green700',
+            primarySolidContrast: '$white',
+            primaryShadow: '$green500',
+
+            gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+            link: '#5E1DAD',
+
+            // you can also create your own color
+            myColor: '#ff4ecd'
+
+            // ...  more colors
+        },
+        space: {},
+        fonts: {}
+    }
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+
+        <NextUIProvider theme={theme}>
+
+
+            <BrowserRouter>
+
+                <Routes>
+                    {/*todo*/}
+
+
+                    <Route path='/employee' element={<EmployeeDashboard/>}/>
+
+                    <Route path='/user' element={<CustomerDashboard/>}/>
+
+                    {/*<Route path='/home' element={<Landing/>}/>*/}
+
+                    <Route path='/' element={<Login/>}/>
+
+
+
+                </Routes>
+            </BrowserRouter>
+        </NextUIProvider>
+
+
+    );
 }
 
 export default App;
