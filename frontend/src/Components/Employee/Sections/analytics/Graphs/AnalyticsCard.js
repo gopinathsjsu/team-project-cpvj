21860175d58ab97642a5b82c9601083a80644e89
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2023-05-02 16:25:15 -0700

    Analytics Card and code reformat

--- frontend/src/Components/Employee/Sections/analytics/Graphs/AnalyticsCard.js
+++ frontend/src/Components/Employee/Sections/analytics/Graphs/AnalyticsCard.js
@@ -1,15 +1,5 @@
 
 
-import AnalyticsCard from "./AnalyticsCard";
-import {Dropdown, Grid, Text} from "@nextui-org/react";
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import WeeklyDayVsClassesLine from "./Graphs/WeeklyDayVsClassesLine";
-import HoursSpentByWeekType from "./Graphs/HoursSpentByWeekType";
-import AnalyticsCards from "./AnalyticsCard";
-import ClassesVsUsers from "./Graphs/ClassesVsUsers";
-import MostVisitedDayHeatMap from "./Graphs/HeatMap/MostVisitedDayHeatMap";
-
 import {Card, Grid, Text} from "@nextui-org/react";
 import React from "react";
 
@@ -25,6 +15,63 @@ const AnalyticsCards = (props)=> {
      *
      * i could've made the cards dynamically, since they are only 3 i resorted to hard coding.
      */
+
+    const data = props.data || []
+
+    return <Grid.Container gap={4}>
+        <Grid xs={4} >
+            <Card isHoverable style={{backgroundImage: "linear-gradient(to right, #4880EC, #019CAD)" }}>
+                <Card.Header>
+
+                    <Grid.Container>
+
+                        <Grid xs={12} justify={"center"}><Text h3>Total Checkin's</Text></Grid>
+                        <Grid xs={4}></Grid>
+                        <Grid xs={4} justify={"center"}><Text h2>{data[0].current}</Text></Grid>
+                        <Grid xs={4}></Grid>
+                        <Grid xs={12} justify={"center"}><Text h4>{data[0].change>0?"Increased by ":"Decreased by"} {data[0].change>0?data[0].change:-1*data[0].change}%</Text></Grid>
+
+                    </Grid.Container>
+
+                </Card.Header>
+            </Card>
+
+        </Grid>
+        <Grid xs={4}>
+            <Card isHoverable style={{backgroundImage: "linear-gradient(to right, #CB356B, #BD3F32)" }}>
+                <Card.Header>
+
+                    <Grid.Container>
+
+                        <Grid xs={12} justify={"center"}><Text h3>New Member's</Text></Grid>
+                        <Grid xs={4}></Grid>
+                        <Grid xs={4} justify={"center"}><Text h2>{data[1].current}</Text></Grid>
+                        <Grid xs={4}></Grid>
+                        <Grid xs={12} justify={"center"}><Text h4>{data[1].change>0?"Increased by ":"Decreased by"} {data[1].change>0?data[1].change:-1*data[1].change}%</Text></Grid>
+
+                    </Grid.Container>
+
+                </Card.Header>
+
+
+            </Card>
+        </Grid>
+        <Grid xs={4}>
+            <Card isHoverable style={{backgroundImage: "linear-gradient(to right, #4568DC, #B06AB3)" }}>
+                <Card.Header>
+
+                    <Grid.Container>
+                        <Grid xs={12} justify={"center"}><Text h3>Enrollment in classes</Text></Grid>
+                        <Grid xs={4}></Grid>
+                        <Grid xs={4} justify={"center"}><Text h2>{data[2].current}</Text></Grid>
+                        <Grid xs={4}></Grid>
+                        <Grid xs={12} justify={"center"}><Text h4>{data[2].change>0?"Increased by ":"Decreased by"} {data[2].change>0?data[2].change:-1*data[2].change}%</Text></Grid>
+                    </Grid.Container>
+
+                </Card.Header>
+            </Card>
+        </Grid>
+    </Grid.Container>
 
 }
 
