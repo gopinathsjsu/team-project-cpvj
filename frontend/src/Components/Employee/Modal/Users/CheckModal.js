546e51c5d56edd965de91f02bee651d7cb38cb1e
priyanka <priyanka1012madas@gmail.com>
2023-05-04 10:44:32 -0700

    Implemented checkIn checkOut logic

--- frontend/src/Components/Employee/Modal/Users/CheckModal.js
+++ frontend/src/Components/Employee/Modal/Users/CheckModal.js
@@ -22,6 +22,23 @@ const CheckModal = forwardRef((props, ref) => {
         isCheckIn: true,
         description: "Enter user email to check them in"
     });
+    const checkIn = () => {
+        setAddClassVisible(true);
+        setLoading(false);
+        setData({name: "Check In User", isCheckIn: true, description: "Enter user email to check them in"});
+    }
+    const checkOut = () => {
+        setAddClassVisible(true);
+        setLoading(false);
+        setData({name: "Check Out User", isCheckIn: false, description: "Enter user email to check them out"});
+    }
+
+
+    const closeHandler = () => {
+        setAddClassVisible(false);
+        setLoading(true);
+
+    };
 
 });
 export default CheckModal;
