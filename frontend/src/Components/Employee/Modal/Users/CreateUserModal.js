b6dc2e5e7c3461fb3cde48f391fbe4b0748514fd
priyanka <priyanka1012madas@gmail.com>
2023-05-08 11:04:12 -0700

    CreateUserModal.js file basic implementation

---
+++ frontend/src/Components/Employee/Modal/Users/CreateUserModal.js
@@ -0,0 +1,9 @@
+import {Button, Checkbox, Dropdown, Grid, Input, Loading, Modal, Text} from "@nextui-org/react";
+import React, {forwardRef, useImperativeHandle, useState} from "react";
+import axios from "axios";
+
+const CreateUserModal = forwardRef(({}, ref) => {
+
+});
+export default CreateUserModal;
+
