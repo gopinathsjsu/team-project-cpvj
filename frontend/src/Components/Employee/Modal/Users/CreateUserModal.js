02c7ab7902032b8f0666976dece932c5680319de
priyanka <priyanka1012madas@gmail.com>
2023-05-09 13:40:27 -0700

    Implemented useImperativeHandle and show

--- frontend/src/Components/Employee/Modal/Users/CreateUserModal.js
+++ frontend/src/Components/Employee/Modal/Users/CreateUserModal.js
@@ -3,7 +3,25 @@ import React, {forwardRef, useImperativeHandle, useState} from "react";
 import axios from "axios";
 
 const CreateUserModal = forwardRef(({}, ref) => {
+    useImperativeHandle(ref, () => {
+        return {
+            addUser: show
+        }
+    })
 
+    const [isTrail, setIsTrail] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    const [visible, setAddClassVisible] = React.useState(false);
+
+    const show = () => {
+        setAddClassVisible(true);
+        setLoading(false);
+
+        // send API CALL
+
+    }
 });
 export default CreateUserModal;
 
