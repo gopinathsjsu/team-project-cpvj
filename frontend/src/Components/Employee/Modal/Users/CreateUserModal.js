39919111e009010c80ba2fefedf4e07de027577b
priyanka <priyanka1012madas@gmail.com>
2023-05-09 21:50:36 -0700

    Added closeHandler and formSubmitHandler code

--- frontend/src/Components/Employee/Modal/Users/CreateUserModal.js
+++ frontend/src/Components/Employee/Modal/Users/CreateUserModal.js
@@ -22,6 +22,25 @@ const CreateUserModal = forwardRef(({}, ref) => {
         // send API CALL
 
     }
+    const closeHandler = () => {
+        setAddClassVisible(false);
+        setLoading(true);
+
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        const data={
+            username :event.target.userName.value,
+            password :event.target.password.value,
+            email:event.target.email.value,
+            istrail:isTrail,
+            membership:isTrail?1: parseInt(event.target.membership.value)
+        }
+
+        setLoading(true);
+        apiCall(data)
+    }
 });
 export default CreateUserModal;
 
